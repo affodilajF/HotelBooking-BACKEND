782a962883ce7f39702719cef799868fd4190154
Affodilaj Fadil <fadhilaainsalsabilla@mail.ugm.ac.id>
2024-04-09 15:47:35 +0700

    test: add user regist success test case

---
+++ src/test/java/HotelBooking/HotelBookingbackendservice/controller/UserControllerTest.java
@@ -0,0 +1,63 @@
+package HotelBooking.HotelBookingbackendservice.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import HotelBooking.HotelBookingbackendservice.model.User.RegisterUserRequest;
+import HotelBooking.HotelBookingbackendservice.model.WebResponse;
+import HotelBooking.HotelBookingbackendservice.repository.UserRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@EnableWebMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp(){
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void testRegisterSuccess() throws Exception {
+        RegisterUserRequest request = new RegisterUserRequest();
+        request.setUsername("test_usn");
+        request.setName("test_name");
+        request.setPassword("test_passwd");
+
+        mockMvc.perform(
+                        post("/api/v1/user-registration")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON_VALUE) // Explicitly set content type
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andExpect(status().isOk())
+                .andDo(result -> {
+                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {});
+                    assertEquals("OK", response.getData());
+                });
+    }
+}
